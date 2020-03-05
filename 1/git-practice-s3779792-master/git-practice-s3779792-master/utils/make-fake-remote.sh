c5203b682bcf66dd74bf6acaa530589764034490
s3779792 <3779702@student.rmit.edu.au>
2020-03-06 08:58:39 +1100

    reset

--- 1/git-practice-s3779792-master/git-practice-s3779792-master/utils/make-fake-remote.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-makefakeremoterepo() {
-    # First cleanup if there is an old exercise repository
-    rm -rf remote/
-
-    # Initialize a new remote repository
-    git init --bare remote
-}
